adb864dfe2bf145daf8c7d7453bf7b769a5ab8e0
Mansi singh <mansi123singh1234@gmail.com>
2024-08-01 10:53:12 +0530

    done

--- src/components/PracticeUseEffect.jsx
+++ src/components/PracticeUseEffect.jsx
@@ -6,26 +6,35 @@ const PracticeUseEffect = () => {
   useEffect(() => {
     fetch("https://fakestoreapi.com/products/")
       .then((res) => res.json())
-      .then((item) => setData(item))
+      .then((items) => setData(items))
       .catch((error) => console.log(error));
   }, []);
 
   console.log(data);
+
   return (
-    <div className=" grid grid-cols-4 w-[90%] h-[80%] w- m-auto gap-9 px-6 py-5 ">
-      {data.map((item, index) => (
-        <div
-          key={index}
-          className=" bg-white-400 rounded-lg shadow-lg px-5 py-5  "
-        >
-          <img className="  w-full h-48 mb-6 " src={item.image} />
-          <h1 className="mb-2 text-center text-red-900">{item.title}</h1>
-          <h2 className="mb-2 font-bold text-center  w-full max-w-xs mx-auto truncate ">
-            {item.description}
-          </h2>
-          <h2 className="text-center font-medium ">Price - {item.price}</h2>
-        </div>
-      ))}
+    <div className="grid grid-cols-4 w-[90%] h-[80%] m-auto gap-9 px-6 py-5">
+      {data.length === 0 ? (
+        <h1>Loading</h1>
+      ) : (
+        data.map((item, index) => (
+          <div
+            key={index}
+            className="bg-white-400 rounded-lg shadow-lg px-5 py-5"
+          >
+            <img
+              className="w-full h-48 mb-6"
+              src={item.image}
+              alt={item.title}
+            />
+            <h1 className="mb-2 text-center text-red-900">{item.title}</h1>
+            <h2 className="mb-2 font-bold text-center w-full max-w-xs mx-auto truncate">
+              {item.description}
+            </h2>
+            <h2 className="text-center font-medium">Price - ${item.price}</h2>
+          </div>
+        ))
+      )}
     </div>
   );
 };
