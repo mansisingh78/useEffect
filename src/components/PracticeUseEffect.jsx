3c5e66099a2362395f7737ae55ae5899ad7c9080
Mansi singh <mansi123singh1234@gmail.com>
2024-08-01 10:27:09 +0530

    done

--- src/components/PracticeUseEffect.jsx
+++ src/components/PracticeUseEffect.jsx
@@ -12,10 +12,18 @@ const PracticeUseEffect = () => {
 
   console.log(data);
   return (
-    <div className="flex-col justify-center text-center p-2 gap-2">
+    <div className=" grid grid-cols-4 w-[90%] h-[80%] w- m-auto gap-9 px-6 py-5 ">
       {data.map((item, index) => (
-        <div key={index}>
-          <h2>{item.title}</h2>
+        <div
+          key={index}
+          className=" bg-white-400 rounded-lg shadow-lg px-5 py-5  "
+        >
+          <img className="  w-full h-48 mb-6 " src={item.image} />
+          <h1 className="mb-2 text-center text-red-900">{item.title}</h1>
+          <h2 className="mb-2 font-bold text-center  w-full max-w-xs mx-auto truncate ">
+            {item.description}
+          </h2>
+          <h2 className="text-center font-medium ">Price - {item.price}</h2>
         </div>
       ))}
     </div>
